fabfe0a4a380afa28bb1d2194567f820b3053a82
ohaluminum <lejinghuang2000@gmail.com>
2021-03-03 23:20:00 -0600

    Book 3 - Chapter 4: Accessing the Internet

---
+++ Program24/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+
+namespace Program24
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*
+             * The NetworkInformation class contains a bunch of information:
+             *  - the status of the network
+             *  - the current IP addresses
+             *  - the gateway being used by the current machine (and more)
+             */
+            if (NetworkInterface.GetIsNetworkAvailable())
+            {
+                toolStripStatusLabel1.Text = "Connected";
+            }
+            else
+            {
+                toolStripStatusLabel1.Text = "Disconnected";
+            }
+
+        }
+    }
+}
